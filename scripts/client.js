e1885871c3be12fcfcc33e4a75ac30baf15d37c9
Jeremy Cady <jcady90@icloud.com>
2021-07-25 18:01:19 -0500

    goals complete

--- scripts/client.js
+++ scripts/client.js
@@ -9,12 +9,17 @@ function onReady() {
     console.log('Ready!');
 
     // Listener for submit button
-    $('#submitButton').on('click', clickSubmit);
+    $(document).on('click', '#submitButton', clickSubmit);
+
+    // Listener for delete button
+    $(document).on('click', '#deleteButton', deleteEmployee);
     
 }
 
 function clickSubmit (event) {
-    //console.log('this is', $(this));
+    console.log('this is', $(this));
+    console.log('event', event);
+    
     console.log('clickSubmit');
     
     // Create variable to store the employee 
@@ -27,39 +32,69 @@ function clickSubmit (event) {
 
     // Clear input fields
     $('.inputField').val('');
-    
+
+    // Render employee objects to the DOM
+    displayEmployeesOnDOM();
+
+    // Sum all salaries
+    let totalSalaries = sumSalaries();
+
+    // Set total salaries
+    $('#salaryTotal').text(totalSalaries.toFixed(2));
 
     
 }
 
+// Function to create employee objects
 function getEmployeeInfo() {
     let employee = {
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
         idNumber: $('#idNum').val(),
         title: $('#title').val(),
-        annualSalary: $('#annualSalary').val(),
+        annualSalary: Number($('#annualSalary').val()),
     };
 
     return employee;
 }
 
+// Function to display employee info in the table on the DOM
 function displayEmployeesOnDOM() {
 
     // empty display to start
-    $('.tableWhole').empty();
+    $('#employeeTable').empty();
 
     // Loop through employees array and display each one on the DOM
-    for (let employee of employees) {
-        $('.tableWhole').append(`
+    for (let employee of employeesArray) {
+        $('.tableRow').append(`
             <tr>
                 <td>${employee.firstName}</td>
                 <td>${employee.lastName}</td>
-                <td>${employee.firstName}</td>
                 <td>${employee.idNumber}</td>
                 <td>${employee.title}</td>
-                <td>${employee.annualSalary.toFixed(2)}</td>
+                <td>${employee.annualSalary}</td>
+                <td><button class="deleteButton">Delete</button></td>
             </tr>
-        `)
+        `);
     }
+}
+
+function sumSalaries() {
+    let totalSalaries = 0;
+    for(employee of employeesArray) {
+        totalSalaries += employee.annualSalary;
+        if (totalSalaries>20000) {
+            $('.totalSalaries').css('background-color', 'red');
+        }
+    }
+
+    return totalSalaries;
+}
+
+function deleteEmployee(event) {
+    console.log('click delete');
+    $(this).closest('tr').remove();
+    console.log('deleted data');
+    
+    
 }
